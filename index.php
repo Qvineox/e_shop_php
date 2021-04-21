ac06aa747d3d70912c3dd9538391eaac4e3c8596
Qvineox <yarlrusman@gmail.com>
2021-04-21 19:32:48 +0300

    Initial configuration

---
+++ index.php
@@ -0,0 +1,83 @@
+<html>
+<title>
+    Главная страница
+</title>
+<style>
+
+</style>
+<link rel="stylesheet" href="styles.css">
+<body>
+<table width="750" cellpadding="5" cellspacing="0">
+    <tr>
+        <td class="header" colspan="3">header</td>
+    </tr>
+    <tr>
+        <td class="left-zone">
+            <ul class="menu" style="margin-right: 30%">
+                <a href="#">
+                    <li class="menu-article home">
+                        Главная<img src="resources/home.svg"></li>
+                </a>
+
+                <a href="#">
+                    <li class="menu-article catalog">
+                        Каталог<img src="resources/flower.svg"></li>
+                </a>
+
+                <a href="#">
+                    <li class="menu-article gallery">
+                        Галерея<img src="resources/gallery.svg"></li>
+                </a>
+
+                <a href="#">
+                    <li class="menu-article contacts">
+                        Контакты<img src="resources/contacts.svg"></li>
+                </a>
+
+                <a href="#">
+                    <li class="menu-article profile">
+
+                        Профиль<img src="resources/profile.svg"></li>
+                </a>
+            </ul>
+        </td>
+        <td class="content center-zone">center</td>
+        <td class="right-zone">
+            <ul class="menu">
+                <a href="#">
+                    <li class="bin-article checkout">
+                        <img src="resources/basket.svg"><span class="bin-summary">Корзина<br>300 руб</span></li>
+                </a>
+            </ul>
+        </td>
+    </tr>
+    <tr class="spacer" style="height: 12px"></tr>
+    <tr>
+        <td class="footer curved" colspan="3">
+            <table>
+                <tr>
+                    <td colspan="2" class="info">
+                        <p class="info" style="opacity: 80%">
+                            МИРЭА, 2021<br>
+                            Лысак Ярослав Денисович, БСБО-09-18</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="info left-footer">
+                        <p class="info" style="text-align: justify; ">Проект: eShop<br>
+                            Версия: 0.01<br>
+                            Публикация: 26.02.2021
+                        </p>
+                    </td>
+                    <td class="info right-footer">
+                        <p class="info" style="text-align: justify; ">
+                            Место для приколов
+                        </p>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+</body>
+</html>
